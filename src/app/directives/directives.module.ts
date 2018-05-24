f7ed7c375b6fc50926221e62c33572586a615d48
naphattharawat <naphattharawat@gmail.com>
2018-05-24 18:36:08 +0700

    ปรับหน้า user

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,11 +1,13 @@
-import { SearchUserComponent } from "./search-user/search-user.component";
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
+import { SearchUserComponent } from './search-user/search-user.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AgxTypeaheadModule } from '@siteslave/agx-typeahead';
+import { AutocompletePeopleComponent } from './autocomplete-people/autocomplete-people.component';
 @NgModule({
   declarations: [
-    SearchUserComponent
+    SearchUserComponent,
+    AutocompletePeopleComponent
   ],
   imports: [
     CommonModule,
@@ -13,7 +15,8 @@ import { AgxTypeaheadModule } from '@siteslave/agx-typeahead';
     AgxTypeaheadModule
   ],
   exports:[
-    SearchUserComponent
+    SearchUserComponent,
+    AutocompletePeopleComponent
   ]
 })
 export class DirectivesModule { }
